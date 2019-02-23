f6fdeac358954e95f094c37827d6c56f8feb80ea
prudencep <temiprudy@gmail.com>
2019-02-22 19:27:45 -0500

    Modified code to allow recipient

--- src/main/java/com/google/codeu/servlets/MessageServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageServlet.java
@@ -32,7 +32,8 @@ import org.jsoup.safety.Whitelist;
 
 /** Handles fetching and saving {@link Message} instances. */
 @WebServlet("/messages")
-public class MessageServlet extends HttpServlet {
+public class MessageServlet extends HttpServlet
+{
 
   private Datastore datastore;
 
@@ -46,13 +47,15 @@ public class MessageServlet extends HttpServlet {
    * an empty array if the user is not provided.
    */
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException
+  {
 
     response.setContentType("application/json");
 
     String user = request.getParameter("user");
 
-    if (user == null || user.equals("")) {
+    if (user == null || user.equals(""))
+    {
       // Request is invalid, return empty array
       response.getWriter().println("[]");
       return;
@@ -67,20 +70,23 @@ public class MessageServlet extends HttpServlet {
 
   /** Stores a new {@link Message}. */
   @Override
-  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException
+  {
 
     UserService userService = UserServiceFactory.getUserService();
-    if (!userService.isUserLoggedIn()) {
+    if (!userService.isUserLoggedIn())
+    {
       response.sendRedirect("/index.html");
       return;
     }
 
     String user = userService.getCurrentUser().getEmail();
     String text = Jsoup.clean(request.getParameter("text"), Whitelist.none());
+    String recipient = request.getParameter("recipient"); //added recipient variable
 
-    Message message = new Message(user, text);
+    Message message = new Message(user, text, recipient); //added recipient as a parameter
     datastore.storeMessage(message);
 
-    response.sendRedirect("/user-page.html?user=" + user);
+    response.sendRedirect("/user-page.html?user=" + recipient); //changed user to recipient so it takes user back to previous page
   }
 }
