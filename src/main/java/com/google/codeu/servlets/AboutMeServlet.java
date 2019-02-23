21a2a0aaae72222a2b29f167f9c9dae3f1ccae63
Kendall Castaneda <kpcasta11@dhcp-wifi-8021x-155-41-112-172.bu.edu>
2019-02-23 17:44:10 -0500

    PR #21: Completed Step 2 for Familiarization Project: About Me.
    Commit AboutMeServlet Step 2
    
    Formatted 4 Servlets using Google Style Guide
    
    Removed new line above response.getOutputStream() and canged comment style to // and moved comment closer to respones.getOutputStream
    
    About Me Servlet with resovled comments and correct Google Style

---
+++ src/main/java/com/google/codeu/servlets/AboutMeServlet.java
@@ -0,0 +1,16 @@
+package com.google.codeu.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/about")
+public class AboutMeServlet extends HttpServlet {
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    // Responds with a hard-coded message for testing purposes.
+    response.getOutputStream().println("hello world");
+  }
+}
