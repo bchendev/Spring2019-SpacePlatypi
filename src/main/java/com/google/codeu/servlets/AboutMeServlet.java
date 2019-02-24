2ab1465af17035b6fae668ae789e8a1a070c2cac
Kendall Castaneda <kpcasta11@dhcp-wifi-8021x-155-41-112-172.bu.edu>
2019-02-24 16:23:57 -0500

    Removed new line above response.getOutputStream() and canged comment style to // and moved comment closer to respones.getOutputStream

--- src/main/java/com/google/codeu/servlets/AboutMeServlet.java
+++ src/main/java/com/google/codeu/servlets/AboutMeServlet.java
@@ -6,13 +6,12 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Responds with a hard-coded message for testing purposes. */
+
 @WebServlet("/about")
 public class AboutMeServlet extends HttpServlet {
-
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-
+  //Responds with a hard-coded message for testing purposes.
     response.getOutputStream().println("hello world");
   }
 }
