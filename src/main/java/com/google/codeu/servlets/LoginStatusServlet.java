ded8779cd3715929b4c3d98cc65813250b077ca5
Kendall Castaneda <kpcasta11@dhcp-wifi-8021x-155-41-112-172.bu.edu>
2019-02-24 15:43:01 -0500

    Formatted 4 Servlets using Google Style Guide

--- src/main/java/com/google/codeu/servlets/LoginStatusServlet.java
+++ src/main/java/com/google/codeu/servlets/LoginStatusServlet.java
@@ -19,16 +19,13 @@ package com.google.codeu.servlets;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.gson.JsonObject;
-
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Returns login data as JSON, e.g. {"isLoggedIn": true, "username": "Ada"}
- */
+/** Returns login data as JSON, e.g. {"isLoggedIn": true, "username": "Ada"} */
 @WebServlet("/login-status")
 public class LoginStatusServlet extends HttpServlet {
 
