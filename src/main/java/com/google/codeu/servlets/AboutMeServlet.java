90264455caa15c4312ef64ce15a68d53e6808951
Kendall Castaneda <kpcasta11@dhcp-wifi-8021x-155-41-112-172.bu.edu>
2019-02-24 18:05:15 -0500

    About Me Servlet with resovled comments and correct Google Style

--- src/main/java/com/google/codeu/servlets/AboutMeServlet.java
+++ src/main/java/com/google/codeu/servlets/AboutMeServlet.java
@@ -6,12 +6,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-
 @WebServlet("/about")
 public class AboutMeServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-  //Responds with a hard-coded message for testing purposes.
+    // Responds with a hard-coded message for testing purposes.
     response.getOutputStream().println("hello world");
   }
 }
