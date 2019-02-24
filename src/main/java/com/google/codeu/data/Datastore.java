6abc9aa6ebab77cd4a559199b1bd4bcad445c08a
Sriya Lingampalli <sriyalingampalli@Sriyas-MacBook-Pro.local>
2019-02-23 22:25:53 -0800

    Can fetch all messages from storage to servlet

--- src/main/java/com/google/codeu/data/Datastore.java
+++ src/main/java/com/google/codeu/data/Datastore.java
@@ -61,6 +61,56 @@ public class Datastore {
             .addSort("timestamp", SortDirection.DESCENDING);
     PreparedQuery results = datastore.prepare(query);
 
+    pMessage(messages, user, results);
+
+    // for (Entity entity : results.asIterable()) {
+    //   try {
+    //     String idString = entity.getKey().getName();
+    //     UUID id = UUID.fromString(idString);
+    //     String text = (String) entity.getProperty("text");
+    //     long timestamp = (long) entity.getProperty("timestamp");
+
+    //     Message message = new Message(id, user, text, timestamp);
+    //     messages.add(message);
+    //   } catch (Exception e) {
+    //     System.err.println("Error reading message.");
+    //     System.err.println(entity.toString());
+    //     e.printStackTrace();
+    //   }
+    // }
+
+    return messages;
+  }
+
+  public List<Message> getAllMessages(){
+    List<Message> messages = new ArrayList<>();
+
+    Query query = 
+        new Query("Message")
+            .addSort("timestamp", SortDirection.DESCENDING);
+    PreparedQuery results = datastore.prepare(query);
+
+    for (Entity entity : results.asIterable()) {
+      try {
+        String idString = entity.getKey().getName();
+        UUID id = UUID.fromString(idString);
+        String user = (String) entity.getProperty("user");
+        String text = (String) entity.getProperty("text");
+        long timestamp = (long) entity.getProperty("timestamp");
+
+        Message message = new Message(id, user, text, timestamp);
+        messages.add(message);
+      } catch (Exception e) {
+        System.err.println("Error reading message.");
+        System.err.println(entity.toString());
+        e.printStackTrace();
+      }
+    }
+
+    return messages;
+  }
+
+  public List pMessage(List messages, String user, PreparedQuery results) {
     for (Entity entity : results.asIterable()) {
       try {
         String idString = entity.getKey().getName();
@@ -76,7 +126,7 @@ public class Datastore {
         e.printStackTrace();
       }
     }
-
     return messages;
   }
+
 }
