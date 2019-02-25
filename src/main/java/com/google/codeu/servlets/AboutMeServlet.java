849bcd0f481b0bfa3006b0dfacd496b4c8a02015
Kendall Castaneda <kpcasta11@dhcp-wifi-8021x-155-41-112-172.bu.edu>
2019-02-24 19:54:45 -0500

    PR #26: Expanded the server logic in About me to open a blank page with a filler email address when you navigate to https://sp19-codeu-28-4865.appspot.com/about?user=Ada@codeustudents.com
    Removed unneccessary new lines, reformatted class comment to javadoc, reworded comment from 'return empty response' to 'No-op if request is invalid', and replace user.equals() with user.isEmpty()
    
    Removed another new line, and localized the string: aboutMe

--- src/main/java/com/google/codeu/servlets/AboutMeServlet.java
+++ src/main/java/com/google/codeu/servlets/AboutMeServlet.java
@@ -1,16 +1,51 @@
 package com.google.codeu.servlets;
 
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.codeu.data.Datastore;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+/** A servlet that handles fetching and saving user data to display the About Me page. */
 @WebServlet("/about")
 public class AboutMeServlet extends HttpServlet {
+
+  private Datastore datastore;
+
+  @Override
+  public void init() {
+    datastore = new Datastore();
+  }
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    // Responds with a hard-coded message for testing purposes.
-    response.getOutputStream().println("hello world");
+    response.setContentType("text/html");
+    String user = request.getParameter("user");
+
+    if (user == null || user.isEmpty()) {
+      // No-op if the request is invalid.
+      return;
+    }
+
+    String aboutMe = String.format("This is %s's about me.", user);
+    response.getOutputStream().println(aboutMe);
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    UserService userService = UserServiceFactory.getUserService();
+    if (!userService.isUserLoggedIn()) {
+      response.sendRedirect("/index.html");
+      return;
+    }
+
+    String userEmail = userService.getCurrentUser().getEmail();
+    System.out.println("Saving about me for " + userEmail);
+    // TODO(kcastaneda): Save the data
+
+    response.sendRedirect("/user-page.html?user=" + userEmail);
   }
 }
