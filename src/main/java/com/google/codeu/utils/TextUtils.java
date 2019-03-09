7d2fcf7a4ddecdc69dd23c42485fc9755a4cfec3
Karen Lin <linkaren.1.2.3@gmail.com>
2019-03-09 17:24:22 -0500

    initial stages for stylized text. Added pretty text editor to user-page.html

---
+++ src/main/java/com/google/codeu/utils/TextUtils.java
@@ -0,0 +1,18 @@
+package com.google.codeu.servlets;
+
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.codeu.data.Datastore;
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+//import org.apache.commons.lang3.StringEscapeUtils;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+
+public class TextUtils{
+
+}
