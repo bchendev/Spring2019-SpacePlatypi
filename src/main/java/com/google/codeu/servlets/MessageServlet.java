26252648b8495398deb975b02977ecfcf55b9594
Karen Lin <linkaren.1.2.3@gmail.com>
2019-03-09 21:40:37 -0500

    added basic cleaning function using Jsoup

--- src/main/java/com/google/codeu/servlets/MessageServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageServlet.java
@@ -76,7 +76,7 @@ public class MessageServlet extends HttpServlet {
     }
 
     String user = userService.getCurrentUser().getEmail();
-    String text = Jsoup.clean(request.getParameter("text"), Whitelist.none());
+    String text = Jsoup.clean(request.getParameter("text"), Whitelist.basic());
     String recipient = request.getParameter("recipient");
 
     Message message = new Message(user, text, recipient);
--- src/main/java/com/google/codeu/utils/TextUtils.java
+++ src/main/java/com/google/codeu/utils/TextUtils.java
@@ -1,18 +1,70 @@
 package com.google.codeu.servlets;
 
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
-import com.google.codeu.data.Datastore;
 import java.io.IOException;
-import javax.servlet.annotation.WebServlet;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 
-//import org.apache.commons.lang3.StringEscapeUtils;
-import org.jsoup.Jsoup;
-import org.jsoup.safety.Whitelist;
+// import org.apache.commons.text.StringEscapeUtils;
 
-public class TextUtils{
+public class TextUtils extends HttpServlet {
+  public void filterDangerousText(HttpServletRequest request) throws ServletException, IOException {
+    // String cleanedText = Jsoup.clean(request.getParameter("text"), Whitelist.basic());
+    // response.sendRedirect(request.getRequestURI());
+    // return cleanedText;
+  }
 
+  public void markdownToHtml(HttpServletRequest request) throws ServletException, IOException {
+    // String enteredText = request.getParameter("text");
+
+    // make sure user didn't enter HTML along with our markup
+    // String escapedText = StringEscapeUtils.escapeHtml4(enteredText);
+
+  }
+  /*
+    /**
+     * Responds with a JSON representation of {@link Message} data for a specific user. Responds with
+     * an empty array if the user is not provided.
+
+    @Override
+    public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+      response.setContentType("application/json");
+
+      String user = request.getParameter("user");
+
+      if (user == null || user.equals("")) {
+        // Request is invalid, return empty array
+        response.getWriter().println("[]");
+        return;
+      }
+
+      List<Message> messages = datastore.getMessages(user);
+      Gson gson = new Gson();
+      String json = gson.toJson(messages);
+
+      response.getWriter().println(json);
+    }
+
+    /** Stores a new {@link Message}.
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+      UserService userService = UserServiceFactory.getUserService();
+      if (!userService.isUserLoggedIn()) {
+        response.sendRedirect("/index.html");
+        return;
+      }
+
+      String user = userService.getCurrentUser().getEmail();
+      String text = Jsoup.clean(request.getParameter("text"), Whitelist.none());
+      String recipient = request.getParameter("recipient");
+
+      Message message = new Message(user, text, recipient);
+      datastore.storeMessage(message);
+
+      response.sendRedirect("/user-page.html?user=" + recipient);
+    }
+
+  */
 }
