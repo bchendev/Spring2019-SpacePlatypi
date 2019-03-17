e64f1a1fbb9f2b6778054b5339d5b5b31e545cb6
prudencep <temiprudy@gmail.com>
2019-03-16 21:32:30 -0700

    fixed stickler error

--- src/main/java/com/google/codeu/servlets/MapsServlet.java
+++ src/main/java/com/google/codeu/servlets/MapsServlet.java
@@ -5,9 +5,9 @@ import java.util.Scanner;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import com.google.gson.Gson;
 import com.google.gson.JsonArray;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Returns Starbucks Locations as an array, e.g. [{"lat": 38.4404675, "lng": -122.7144313}]
@@ -17,39 +17,39 @@ public class MapsServlet extends HttpServlet {
 
   JsonArray starbucksLocationsArray;
 
- @Override
- public void init() {
-  starbucksLocationsArray = new JsonArray();
-  Gson gson = new Gson();
-  Scanner scanner = new Scanner(getServletContext().getResourceAsStream("/WEB-INF/starbucks-data.csv"));
-  while(scanner.hasNextLine()) {
-   String line = scanner.nextLine();
-   String[] cells = line.split(",");
+  @Override
+  public void init() {
+    starbucksLocationsArray = new JsonArray();
+    Gson gson = new Gson();
+    Scanner scanner = new Scanner(getServletContext().getResourceAsStream("/WEB-INF/starbucks-data.csv"));
+    while(scanner.hasNextLine()) {
+      String line = scanner.nextLine();
+      String[] cells = line.split(",");
 
-   String state = cells[0];
-   double lat = Double.parseDouble(cells[1]);
-   double lng = Double.parseDouble(cells[2]);
+      String state = cells[0];
+      double lat = Double.parseDouble(cells[1]);
+      double lng = Double.parseDouble(cells[2]);
 
-   starbucksLocationsArray.add(gson.toJsonTree(new UfoSighting(state, lat, lng)));
+      starbucksLocationsArray.add(gson.toJsonTree(new UfoSighting(state, lat, lng)));
+    }
+    scanner.close();
   }
-  scanner.close();
- }
 
- @Override
- public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-  response.setContentType("application/json");
-  response.getOutputStream().println(starbucksLocationsArray.toString());
- }
-
- private static class UfoSighting{
-  String state;
-  double lat;
-  double lng;
-
-  private UfoSighting(String state, double lat, double lng) {
-   this.state = state;
-   this.lat = lat;
-   this.lng = lng;
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    response.setContentType("application/json");
+    response.getOutputStream().println(starbucksLocationsArray.toString());
   }
- }
+ 
+  private static class UfoSighting{
+    String state;
+    double lat;
+    double lng;
+
+   private UfoSighting(String state, double lat, double lng) {
+     this.state = state;
+     this.lat = lat;
+     this.lng = lng;
+   }
+  }
 }
