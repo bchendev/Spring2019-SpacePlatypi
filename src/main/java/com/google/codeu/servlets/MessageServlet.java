0dc185e12f6125591b587e6e6f307f472cba4b0d
Kendall Castaneda <kpcasta11@dhcp-wifi-8021x-155-41-8-163.bu.edu>
2019-03-18 01:11:58 -0400

    Updated the  user-page-loader.js to reflect the new sentimentScore and also fixed MessageServlet import statements

--- src/main/java/com/google/codeu/servlets/MessageServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageServlet.java
@@ -18,6 +18,10 @@ package com.google.codeu.servlets;
 
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
+import com.google.cloud.language.v1.Document;
+import com.google.cloud.language.v1.Document.Type;
+import com.google.cloud.language.v1.LanguageServiceClient;
+import com.google.cloud.language.v1.Sentiment;
 import com.google.codeu.data.Datastore;
 import com.google.codeu.data.Message;
 import com.google.gson.Gson;
