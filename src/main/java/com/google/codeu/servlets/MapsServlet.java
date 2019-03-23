27052a5504a0389d429e9b839a11eb7681bbdabc
prudencep <temiprudy@gmail.com>
2019-03-22 21:04:28 -0400

    added HTML file and made changes to the servlet

--- src/main/java/com/google/codeu/servlets/MapsServlet.java
+++ src/main/java/com/google/codeu/servlets/MapsServlet.java
@@ -9,29 +9,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+
 /**
- * Returns Starbucks Locations as an array, e.g. [{"lat": 38.4404675, "lng": -122.7144313}]
+ * Returns Starbucks Locations a JSON array, e.g. [{"lat": 38.4404675, "lng": -122.7144313}]
  */
+
 @WebServlet("/starbucks-data")
 public class MapsServlet extends HttpServlet {
-
   JsonArray starbucksLocationsArray;
+  
+  public SbLocation  parseFile(String line){
+      String[] cells = line.split(",");
+      String country = cells[0];
+      double lat = Double.parseDouble(cells[1]);
+      double lng = Double.parseDouble(cells[2]);
+
+      SbLocation loc = new SbLocation(country, lat, lng);
+      return loc;
+  }
 
   @Override
   public void init() {
     starbucksLocationsArray = new JsonArray();
+    new SbLocation();
     Gson gson = new Gson();
-    Scanner scanner =
-            new Scanner(getServletContext().getResourceAsStream("/WEB-INF/starbucks-data.csv"));
-    while (scanner.hasNextLine()) {
-      String line = scanner.nextLine();
-      String[] cells = line.split(",");
-
-      String state = cells[0];
-      double lat = Double.parseDouble(cells[1]);
-      double lng = Double.parseDouble(cells[2]);
-
-      starbucksLocationsArray.add(gson.toJsonTree(new UfoSighting(state, lat, lng)));
+    String line = "";
+    Scanner scanner = new Scanner(getServletContext().getResourceAsStream("/WEB-INF/starbucks-data.csv"));
+      while (scanner.hasNextLine()) {
+        line = scanner.nextLine();
+        starbucksLocationsArray.add(gson.toJsonTree(parseFile(line)));
     }
     scanner.close();
   }
@@ -42,13 +48,19 @@ public class MapsServlet extends HttpServlet {
     response.getOutputStream().println(starbucksLocationsArray.toString());
   }
 
-  private static class UfoSighting {
-    String state;
+  private static class SbLocation {
+    String country;
     double lat;
     double lng;
-
-    private UfoSighting(String state, double lat, double lng) {
-      this.state = state;
+    
+   private SbLocation(){
+     country = "";
+     lat = 0;
+     lng = 0;
+    }
+    
+    private SbLocation(String country, double lat, double lng) {
+      this.country = country;
       this.lat = lat;
       this.lng = lng;
     }
