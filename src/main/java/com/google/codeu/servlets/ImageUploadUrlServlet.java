18f6a9fb6700cfd3fc6b42ba773de4ed392f4089
Sriya Lingampalli <sriyalingampalli@sriyas-mbp.attlocal.net>
2019-04-07 15:46:43 -0700

    User can upload file from computer

--- src/main/java/com/google/codeu/servlets/ImageUploadUrlServlet.java
+++ src/main/java/com/google/codeu/servlets/ImageUploadUrlServlet.java
@@ -8,18 +8,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Provides access to a URL that allows a user to upload an image to Blobstore.
- */
+/** Provides access to a URL that allows a user to upload an image to Blobstore. */
 @WebServlet("/image-upload-url")
 public class ImageUploadUrlServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
-  	// Gets the file upload URL
+    // Gets the file upload URL
     BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
-    String uploadUrl = blobstoreService.createUploadUrl("/messages") ;
+    String uploadUrl = blobstoreService.createUploadUrl("/messages");
 
     response.setContentType("text/html");
     response.getOutputStream().println(uploadUrl);
