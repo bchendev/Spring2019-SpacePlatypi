081ebeafc212f8fb0788281bedd2eba5df1302cb
Kendall Castaneda <kpcasta11@dhcp-wifi-8021x-155-41-23-14.bu.edu>
2019-04-07 18:20:20 -0400

    Starter code for Search Bar Functionality

---
+++ src/main/java/com/google/codeu/servlets/SearchBarServlet.java
@@ -0,0 +1,64 @@
+package com.google.codeu.servlets;
+
+import java.io.IOException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import com.google.codeu.data.Datastore;
+import com.google.codeu.data.User;
+import javax.servlet.annotation.WebServlet;
+import org.jsoup.Jsoup;
+import org.jsoup.safety.Whitelist;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
+
+
+/** A servlet that handles the input of a search query and the redirection to the results page */
+@WebServlet("/search")
+public class SearchBarServlet extends HttpServlet {
+  	private Datastore datastore;
+
+  	@Override
+  	public void init() {
+    	datastore = new Datastore();
+  	}
+
+  	@Override 
+  	public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException{
+
+  		//first, take the request and compare it to users stored in datastore
+    		//whitelist is important to make sure nothing can be written to break out code 
+    	//then, whichever users match the input, send them to /search-results.html
+    		//i think it would be a good idea to use a loop here because we want to look at multiple users in the long run
+    	//how are users stored in datastore? what would be the best search algorithim 
+  		User userData = datastore.getUser(user);	  		
+  		response.setContentType("text/html");
+  		String query = request.getParemeter("input"); //im unsure about how get paremeter works 
+
+  		if (query == null || query.isEmpty()) {
+      		// No-op if the request is invalid.
+      		return;
+   		}
+
+   		if (query = userData){
+   			return;
+   		}
+   		
+  	}
+  	@Override
+  	public  void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+  		//gonna redirect the user the the results page
+    	UserService userService = UserServiceFactory.getUserService();
+    	if (!userService.isUserLoggedIn()) {
+      		response.sendRedirect("/index.html");
+      		return;
+    	}
+
+    	//sendRedirect to search-results.html
+
+
+		response.sendRedirect("/search-results.html");
+  	}
+  }
