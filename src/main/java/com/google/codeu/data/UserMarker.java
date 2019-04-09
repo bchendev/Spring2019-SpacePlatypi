046a6adbce6ef77b525ec48bcc4c5efc47582286
prudencep <temiprudy@gmail.com>
2019-04-09 16:13:16 -0400

    javadoc changes

--- src/main/java/com/google/codeu/data/UserMarker.java
+++ src/main/java/com/google/codeu/data/UserMarker.java
@@ -1,26 +1,35 @@
 package com.google.codeu.data;
 
+/** Represents a user generated marker on a map. */
 public class UserMarker {
 
   private double lat;
   private double lng;
   private String content;
 
-  /** UserMarker constructor.*/
+  /**
+ * Constructs a map marker.
+ * @param lat latitude coordinate system
+ * @param lng longitutde coordinate system
+ * @param content text displayed on the map marker
+ */
   public UserMarker(double lat, double lng, String content) {
     this.lat = lat;
     this.lng = lng;
     this.content = content;
   }
-
+  
+  /** Returns the latitude coordinates of the map marker. */
   public double getLat() {
     return lat;
   }
-
+  
+  /** Returns the longitude coordinates of the map marker. */
   public double getLng() {
     return lng;
   }
-
+  
+  /** Returns the content text displayed by the map marker. */
   public String getContent() {
     return content;
   }
