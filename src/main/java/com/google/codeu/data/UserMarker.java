8c2c3acd53018efe12d563f638f8a38e702d0092
prudencep <temiprudy@gmail.com>
2019-04-09 12:00:33 -0400

    geocoding first steps

---
+++ src/main/java/com/google/codeu/data/UserMarker.java
@@ -0,0 +1,27 @@
+package com.google.codeu.data;
+
+public class UserMarker {
+
+  private double lat;
+  private double lng;
+  private String content;
+
+  /** UserMarker constructor.*/
+  public UserMarker(double lat, double lng, String content) {
+    this.lat = lat;
+    this.lng = lng;
+    this.content = content;
+  }
+
+  public double getLat() {
+    return lat;
+  }
+
+  public double getLng() {
+    return lng;
+  }
+
+  public String getContent() {
+    return content;
+  }
+}
