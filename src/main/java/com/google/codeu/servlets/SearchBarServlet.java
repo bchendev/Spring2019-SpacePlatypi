dc342a265daa7e2b0dcea8e07e4a51636edccdf2
Kendall Castaneda <kpcasta11@dhcp-wifi-8021x-168-122-3-129.bu.edu>
2019-04-14 23:57:22 -0400

    edited datastore and searchBarServlet to traverse through all users and return the json version to results-page-loader.js

--- src/main/java/com/google/codeu/servlets/SearchBarServlet.java
+++ src/main/java/com/google/codeu/servlets/SearchBarServlet.java
@@ -3,12 +3,15 @@ package com.google.codeu.servlets;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.codeu.data.Datastore;
+import com.google.gson.Gson;
 import com.google.codeu.data.User;
 import java.io.IOException;
+import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 
 /** A servlet that handles the input of a search query and the redirection to the results page. */
 @WebServlet("/search")
@@ -20,17 +23,28 @@ public class SearchBarServlet extends HttpServlet {
     datastore = new Datastore();
   }
 
+  /**
+   * Responds with a JSON representation of {@link User} data for a search query. Responds with
+   * an empty array if the no user found.
+   */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    
-    response.setContentType("applications/json");
 
-    List<User> users = datastore.getUser(request);
+    response.setContentType("applications/json");
+    String searchQuery = request.getParameter("query");
+
+    List<User> allUsers = datastore.getUsers(searchQuery);
+    List<User> matchingUsers = new ArrayList<User>();
+
+    for(int i =0; i <= allUsers.size(); i++){
+      if (allUsers.get(i).equals(searchQuery)){
+        matchingUsers.add(allUsers.get(i));
+      }
+    }
     Gson gson = new Gson();
-    String json = gson.toJson(users);
+    String json = gson.toJson(matchingUsers);
 
     response.getWriter().println(json);
-    }
   }
 
   @Override
@@ -43,3 +57,4 @@ public class SearchBarServlet extends HttpServlet {
     String query = request.getParameter("query");
     response.sendRedirect("/search-results.html?query=" + query);
   }
+}
