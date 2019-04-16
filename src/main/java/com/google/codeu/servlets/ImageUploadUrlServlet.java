cc4e04f4b7b22e62d90cf7edbb4f5db9606c77c1
Sriya Lingampalli <sriyalingampalli@eduroam-169-233-235-103.ucsc.edu>
2019-04-16 15:38:44 -0700

    Changes to upload feature

--- src/main/java/com/google/codeu/servlets/ImageUploadUrlServlet.java
+++ src/main/java/com/google/codeu/servlets/ImageUploadUrlServlet.java
@@ -7,6 +7,9 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+
 
 /** Provides access to a URL that allows a user to upload an image to Blobstore. */
 @WebServlet("/image-upload-url")
@@ -17,8 +20,12 @@ public class ImageUploadUrlServlet extends HttpServlet {
     // Gets the file upload URL
     BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
     String uploadUrl = blobstoreService.createUploadUrl("/messages");
+    UserService userService = UserServiceFactory.getUserService();
+    String userEmail = userService.getCurrentUser().getEmail();
 
     response.setContentType("text/html");
     response.getOutputStream().println(uploadUrl);
+    response.sendRedirect("/user-page.html?user=" + userEmail);
+
   }
 }
--- src/main/webapp/js/user-page-loader.js
+++ src/main/webapp/js/user-page-loader.js
@@ -19,9 +19,9 @@ const urlParams = new URLSearchParams(window.location.search);
 const parameterUsername = urlParams.get('user');
 
 // URL must include ?user=XYZ parameter. If not, redirect to homepage.
-if (!parameterUsername) {
-  window.location.replace('/');
-}
+// if (!parameterUsername) {
+//   window.location.replace('/');
+// }
 
 /** Sets the page title based on the URL parameter username. */
 function setPageTitle() {
