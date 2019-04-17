08cedb2384e27981a913a39c0ea472e285023404
SriyaLingampalli <36239050+SriyaLingampalli@users.noreply.github.com>
2019-04-16 21:37:42 -0700

    Profile Picture Image Upload (#116)
    * Able to upload image
    * Saves to datastore
    * Submit button refreshes page after uploading to datastore
    * Added URL to user class
    * Changes a profile picture's image source
    * Format errors fixed

--- src/main/java/com/google/codeu/servlets/ImageUploadUrlServlet.java
+++ src/main/java/com/google/codeu/servlets/ImageUploadUrlServlet.java
@@ -1,8 +1,11 @@
 package com.google.codeu.servlets;
 
+import com.google.appengine.api.blobstore.BlobKey;
 import com.google.appengine.api.blobstore.BlobstoreService;
 import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,10 +18,17 @@ public class ImageUploadUrlServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     // Gets the file upload URL
-    BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
-    String uploadUrl = blobstoreService.createUploadUrl("/messages");
+    // BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+    // UserService userService = UserServiceFactory.getUserService();
+    // String userEmail = userService.getCurrentUser().getEmail();
+    // String uploadUrl = blobstoreService.createUploadUrl("/user-page.html?user=" + userEmail);
 
-    response.setContentType("text/html");
-    response.getOutputStream().println(uploadUrl);
+    BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+    Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(request);
+    List<BlobKey> blobKeys = blobs.get("image");
+    // response.setContentType("text/html");
+    // response.getOutputStream().println(uploadUrl);
+    // response.sendRedirect("/user-page.html?user=" + userEmail);
+
   }
 }
--- src/main/java/com/google/codeu/servlets/MessageServlet.java
+++ src/main/java/com/google/codeu/servlets/MessageServlet.java
@@ -16,6 +16,12 @@
 
 package com.google.codeu.servlets;
 
+import com.google.appengine.api.blobstore.BlobKey;
+import com.google.appengine.api.blobstore.BlobstoreService;
+import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+import com.google.appengine.api.images.ImagesService;
+import com.google.appengine.api.images.ImagesServiceFactory;
+import com.google.appengine.api.images.ServingUrlOptions;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
 import com.google.cloud.language.v1.Document;
@@ -31,6 +37,7 @@ import com.google.codeu.data.Message;
 import com.google.gson.Gson;
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -83,6 +90,10 @@ public class MessageServlet extends HttpServlet {
       return;
     }
 
+    BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
+    Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(request);
+    List<BlobKey> blobKeys = blobs.get("image");
+
     // Allows only basic text editing, image uploading, and linking functions
     Whitelist whitelist = Whitelist.basicWithImages().addTags("a").addAttributes("a", "href");
     String text = Jsoup.clean(request.getParameter("text"), whitelist);
@@ -98,6 +109,16 @@ public class MessageServlet extends HttpServlet {
     // String textWithImagesReplaced = userText.replaceAll(regex, replacement);
 
     Message message = new Message(user, text, recipient, sentimentScore);
+
+    if (blobKeys != null && !blobKeys.isEmpty()) {
+      BlobKey blobKey = blobKeys.get(0);
+      ImagesService imagesService = ImagesServiceFactory.getImagesService();
+      System.out.print("hello");
+      ServingUrlOptions options = ServingUrlOptions.Builder.withBlobKey(blobKey);
+      String imageUrl = imagesService.getServingUrl(options);
+      message.setImageUrl(imageUrl);
+    }
+
     datastore.storeMessage(message);
 
     response.sendRedirect("/user-page.html?user=" + recipient);
