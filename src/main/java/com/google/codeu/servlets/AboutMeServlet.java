762b66ff2eb361fa2fa53dc43b662a9d50205787
prudencep <temiprudy@gmail.com>
2019-04-20 18:11:17 -0400

    removed Brian's name

--- src/main/java/com/google/codeu/servlets/AboutMeServlet.java
+++ src/main/java/com/google/codeu/servlets/AboutMeServlet.java
@@ -26,7 +26,6 @@ public class AboutMeServlet extends HttpServlet {
 
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    System.out.println("Brian");
     response.setContentType("text/html");
     String user = request.getParameter("user");
 
