ffd7f67655d5c1deacbe2554d3729e9771f321f2
Sriya Lingampalli <sriyalingampalli@sriyas-mbp.attlocal.net>
2019-04-20 11:50:55 -0700

    Added ImageURL set function and ProfileImageServlet gets Blobstore images

--- src/main/java/com/google/codeu/servlets/ProfileImageServlet.java
+++ src/main/java/com/google/codeu/servlets/ProfileImageServlet.java
@@ -1,29 +1,59 @@
 package com.google.codeu.servlets;
 
+import com.google.appengine.api.blobstore.BlobKey;
 import com.google.appengine.api.blobstore.BlobstoreService;
 import com.google.appengine.api.blobstore.BlobstoreServiceFactory;
+import com.google.appengine.api.images.ImagesService;
+import com.google.appengine.api.images.ImagesServiceFactory;
+import com.google.appengine.api.images.ServingUrlOptions;
 import com.google.appengine.api.users.UserService;
 import com.google.appengine.api.users.UserServiceFactory;
+import com.google.codeu.data.Datastore;
+import com.google.codeu.data.User;
 import java.io.IOException;
+import java.util.List;
+import java.util.Map;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 /** Provides access to a URL that allows a user to upload an image to Blobstore. */
-@WebServlet("/image-upload-profile")
+@WebServlet("/store-image")
 public class ProfileImageServlet extends HttpServlet {
 
+  private Datastore datastore;
+
   @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+  public void init() {
+    datastore = new Datastore();
+  }
+
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    System.out.println("--------------ProfileImageServlet-------------------");
+    UserService userService = UserServiceFactory.getUserService();
+    if (!userService.isUserLoggedIn()) {
+      return;
+    }
+    System.out.println("--------------User Logged In-------------------");
+
+    String email = userService.getCurrentUser().getEmail();
+    User user = datastore.getUser(email);
 
     BlobstoreService blobstoreService = BlobstoreServiceFactory.getBlobstoreService();
-    String uploadUrl = blobstoreService.createUploadUrl("/about");
-    UserService userService = UserServiceFactory.getUserService();
-    String userEmail = userService.getCurrentUser().getEmail();
+    Map<String, List<BlobKey>> blobs = blobstoreService.getUploads(request);
+    List<BlobKey> blobKeys = blobs.get("image");
 
-    // response.setContentType("text/html");
-    // response.getOutputStream().println("/user-page.html?user=");
-    response.sendRedirect("/user-page.html?user=" + userEmail);
+    if (blobKeys != null && !blobKeys.isEmpty()) {
+      System.out.println("--------------Blob key found-------------------");
+      BlobKey blobKey = blobKeys.get(0);
+      ImagesService imagesService = ImagesServiceFactory.getImagesService();
+      ServingUrlOptions options = ServingUrlOptions.Builder.withBlobKey(blobKey);
+      String imageUrl = imagesService.getServingUrl(options);
+      user.setImageUrl(imageUrl);
+      datastore.storeUser(user);
+    }
+    response.sendRedirect("/user-page.html?user=" + email);
   }
 }
